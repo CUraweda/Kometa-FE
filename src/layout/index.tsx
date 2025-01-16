f8bd42121b2e4443a4c5e776441cdc87a461034b
Nur Cahyanto <nur.razdan@gmail.com>
2025-01-16 15:14:18 +0700

    update

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar.component";
+<<<<<<< Updated upstream
+=======
+import bg from "../assets/content/kometa-bg.png"
+>>>>>>> Stashed changes
 
 function Layout() {
   return (
     <>
+<<<<<<< Updated upstream
 
 
       <div className="w-full min-h-screen relative ">
@@ -13,6 +18,16 @@ function Layout() {
             <Outlet />
           </div>
         
+=======
+      <div className="w-full min-h-screen relative ">
+        <img src={ bg} alt="" className="absolute top-0 left-0 w-full h-full"/>
+        <div className="absolute top-0 left-0 w-full h-full" >
+          <Navbar />
+          <Outlet />
+
+        </div>
+
+>>>>>>> Stashed changes
 
       </div>
 
